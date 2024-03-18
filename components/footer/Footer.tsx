317190998716ba0dd310c25a20f21107092132b9
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2024-03-18 22:41:37 +0630

    feat : update footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,11 +4,10 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className='border-t py-[20px] lg:px-28 px-5 flex justify-between items-center md:!flex-row !flex-col md:space-y-0 space-y-[15px]'>
-            <Link href={"/"}>
-                <h1 className='text-xl font-bold flex-1'>NewsPOP</h1>
+            <Link href={"/"} className='flex-1'>
+                <h1 className='text-xl font-bold'>NewsPOP</h1>
             </Link>
-            <p className='text-sm text-gray-500 flex-2 text-center'>&copy; 2024 Your Company. All rights reserved.</p>
-
+            <p className='text-sm text-gray-500 flex-2 text-center'>&copy; {new Date().getFullYear()} NewsPOP. All rights reserved.</p>
             <p className='text-sm flex-1 text-end'>This app is createdy by <a className='underline' href="https://github.com/Kaungmyatkyaw2">Kaung Myat Kyaw</a></p>
         </div>
     )
